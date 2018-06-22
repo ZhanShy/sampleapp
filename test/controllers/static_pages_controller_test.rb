e5c1de56eb7cf2e893c10fce3422bf78018ce292
ZhanShy <YOUR@EMAIL.com>
2018-06-22 16:43:58 +0600

    Make a basic User model (including secure passwords)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -29,7 +29,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "Contact | #{@base_title}"
   end
 
-  test "should get contact" do
+  test "should get signup" do
     get signup_path
     assert_response :success
     assert_select "title", "Signup | #{@base_title}"
